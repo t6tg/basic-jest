867cb49084daa1c86008a2583e513fbfc7b4f0cc
thanawatgulati <thanawatgulatijames@gmail.com>
2020-03-18 13:43:17 +0700

    ⚰️ Object Assignment

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,13 @@ const sum = require('./sum');
 test('adds 2 + 3  equals 5', () => {
   expect(sum(2, 3)).toBe(5);
 });
+
+test('adds 2 + 2 equal 4 ', () => {
+  expect(2 + 2).toBe(4);
+});
+
+test('Object Assignment', () => {
+  const data = { one: 1 };
+  data['two'] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
